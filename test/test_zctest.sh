2f714adec95e34274e5b457c6f5426dd596551a1
Juanjo Garcia-Ripoll <jjgarcia@users.sf.net>
2013-10-13 21:24:59 +0200

    Fixed typo in test script

--- test/test_zctest.sh
+++ test/test_zctest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Testing COMPLEX-COMPLEX16 LAPACK protoype linear equation routines
-exec ./test_linzc < $CBLAPACKDIR/test/input/zctest.in
+exec ./test_lincz < $CBLAPACKDIR/test/input/zctest.in
